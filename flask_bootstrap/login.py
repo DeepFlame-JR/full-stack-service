9b11e7f15f12fd42d3a139a18e534e3ed17388db
JunRyeol <wnsfuf0121@naver.com>
2023-05-08 19:25:06 +0900

    [Flask] Login page with Bootstrap

--- flask_bootstrap/login.py
+++ flask_bootstrap/login.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify, request, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/login')
 def login():
-    username = request.args.get('user_name')  # URI에 있는 path parameter를 가져온다 ?user_name=<name>
-    if username == 'aiden':  # /login?user_name=aiden
+    address = request.args.get('address')
+    passwd = request.args.get('passwd')
+    print(address, passwd)
+    
+    if address == 'aiden@gmail.com':  # /login?user_name=aiden
         return_data = {'auth':True}
     else:
         return_data = {'auth':False}
     return jsonify(return_data)
 
-@app.route('/html_test')
+
+@app.route('/login_html')
 def hello_html():
-    return render_template('login.html')
+    return render_template('login_index.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = '8089')
